e37b23cda66c5141c0c8249e42c2d4ec3f4ac994
kartikchoubey <chaubeykartik@gmail.com>
2019-04-04 16:18:55 +0530

    pulling

--- src/app/user-panel/user-panel.component.ts
+++ src/app/user-panel/user-panel.component.ts
@@ -15,24 +15,24 @@ export class UserPanelComponent implements OnInit {
   BookDetail:Book[]
   constructor(private db:AngularFirestore ,private appService:AppService) { }
   myOption=[
-   { option:'CS',value:'cs'},
-   { option:'EC',value:'ec'},
-   { option:'EX',value:'ex'},
-   { option:'ME',value:'me'},
-   { option:'FT',value:'ft'},
-   { option:'CM',value:'cm'}
-  ]
-  semester=[
-    { option:'1',value:'1'},
-    { option:'2',value:'2'},
-    { option:'3',value:'3'},
-    { option:'4',value:'4'},
-    { option:'5',value:'5'},
-    { option:'6',value:'6'},
-    { option:'7',value:'7'},
-    { option:'8',value:'8'},
-
+    { option:'CS',value:'cs'},
+    { option:'EC',value:'ec'},
+    { option:'EX',value:'ex'},
+    { option:'ME',value:'me'},
+    { option:'FT',value:'ft'},
+    { option:'CM',value:'cm'}
    ]
+   semester=[
+     { option:'1',value:'1'},
+     { option:'2',value:'2'},
+     { option:'3',value:'3'},
+     { option:'4',value:'4'},
+     { option:'5',value:'5'},
+     { option:'6',value:'6'},
+     { option:'7',value:'7'},
+     { option:'8',value:'8'},
+ 
+    ]
 
   ngOnInit() {
    
